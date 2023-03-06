a6234062e01aea7da9be6f23aae90f6e4a691389
Beurre-moutarde <christopher.m.lebreton@gmail.com>
2023-03-06 14:00:20 +0100

    added utils folder with auth.js and helper.js

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const helpers = require('./utils/helpers');
 
+
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
@@ -29,7 +30,7 @@ const sess = {
     })
   };
   
-  app.use(session(sess));
+app.use(session(sess));
 
   // Inform Express.js on which template engine to use
 app.engine('handlebars', hbs.engine);
