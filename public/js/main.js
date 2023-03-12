a3560664f60e43e7133aed246b9193f5b3d15021
Chenghao Hou <houchenghao@hotmail.com>
2023-03-12 22:48:49 +1100

    link homepage login&signup button

---
+++ public/js/main.js
@@ -0,0 +1,15 @@
+const loginHandler = async (event) => {
+    event.preventDefault();
+    document.location.assign('/login');
+};
+
+const signupHandler = async (event) => {
+  event.preventDefault();
+  document.location.assign('/signup');
+};
+
+
+
+document.querySelector('.login-button').addEventListener('click', loginHandler);
+
+document.querySelector('.signup-button').addEventListener('click', signupHandler);
